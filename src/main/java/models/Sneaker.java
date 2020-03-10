0da84047a26df3807e701729fac90479f10986ab
sandeep <snaraya@zipcodes-MBP-9.fios-router.home>
2020-03-09 23:06:00 -0400

    Constructor set for Sneaker

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -6,11 +6,71 @@ public class Sneaker {
     private String name;
     private String brand;
     private String sport;
-    private int size;
+    //private int size;
     private int qty;
     private float price;
 
     public Sneaker(){}
+
+    public Sneaker(int id, String name, String brand, String sport, int qty, float price) {
+
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.sport = sport;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public void setId(int id){
+
+        this.id = id;
+    }
+
+    public int getId(){
+
+        return id;
+    }
+
+    public void setBrand(String band){
+
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+
+        return brand;
+    }
+
+    public void setSport(String sport){
+
+        this.sport = sport;
+    }
+
+    public String getSport(){
+
+        return sport;
+    }
+
+    public void setQty(int qty){
+
+        this.qty = qty;
+    }
+
+    public int getQty(){
+
+        return qty;
+    }
+
+    public void setPrice(float price){
+
+        this.price = price;
+    }
+
+    public float getPrice(){
+
+        return price;
+    }
 
     public void setName(String name){
 
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -19,4 +19,29 @@ public class SneakerTest {
         // then (3)
         Assertions.assertEquals(expected, testSneaker.getName());
     }
+
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedId = 6;
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennnis";
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+
+        // (3)
+        Sneaker testSneaker = new Sneaker(expectedId, expectedName, expectedBrand,
+                expectedSport, expectedQty,expectedPrice);
+
+        // (4)
+        Assertions.assertEquals(expectedId, testSneaker.getId());
+        Assertions.assertEquals(expectedName, testSneaker.getName());
+        Assertions.assertEquals(expectedBrand, testSneaker.getBrand());
+        Assertions.assertEquals(expectedSport, testSneaker.getSport());
+        Assertions.assertEquals(expectedQty, testSneaker.getQty());
+        Assertions.assertEquals(expectedPrice, testSneaker.getPrice());
+    }
 }
+
